89168988e6c9b45023ac406a5746c0c9ae11e194
Wordluc <40872910+Wordluc@users.noreply.github.com>
2023-10-18 20:04:59 +0200

    inserimento required in command ticket

--- Core/Commands/CreateTicket/CreateTicketCommand.cs
+++ Core/Commands/CreateTicket/CreateTicketCommand.cs
@@ -7,16 +7,16 @@ namespace Core.Commands.CreatePassegger
     public enum TycketClass { First, Second }
     internal class CreateTicketCommand : Command<Result<TicketResult>>
     {
-        public string IdRoute { get;init; }
+        public required string IdRoute { get;init; }
         public List<CreateLuggageCommand>? Luggages { get; init; }
-        public TycketClass TypeTicket { get; init; }
-        public string IdAccount {  get; init; } 
+        public required TycketClass TypeTicket { get; init; }
+        public required string IdAccount {  get; init; } 
     }
 
     class CreateLuggageCommand
     {
-        public float Peso { get; init; }
-        public float Dimensione { get; init; }
+        public required float Peso { get; init; }
+        public required float Dimensione { get; init; }
     }
    
 }
